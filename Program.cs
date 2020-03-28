8daa5b58796de959031360a4f508832bbdf37e73
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-03-28 19:45:59 +0500

    Klass Work

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyprojecsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+                             //Задача 1
+            int i = 0;
+            while (i <= 5)
+            {
+            Console.WriteLine($"Где родился Абуабдуллох Рудаки?");
+         string S=Console.ReadLine();
+            if(S=="Панджрудак")
+            {
+                System.Console.WriteLine("Правилный ответ!!!");
+                i=6;
+            }
+            else
+            {
+                System.Console.WriteLine("Вы ошиблись!!!");
+                Console.WriteLine($"У вас осталось {5-i} попитки");
+                i++;
+                if(i==5)System.Console.WriteLine("Вы не смогли ответит на этот вопрос!!!");
+            }
+        }*/
+        /*
+                                //Задача 2
+
+            int A, B, S = 0;
+            Console.Write("A = ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("B = ");
+            B = int.Parse(Console.ReadLine());
+
+            for (int i = A; i <= B; i++) S += i;
+            System.Console.WriteLine($"Sum[{A} до {B}] = {S}");
+            Console.ReadKey();*/
+
+        }
+    }
+}
